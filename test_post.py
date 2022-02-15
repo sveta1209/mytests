09dd5c685f17f7fc7ebddf2dfaff16f91e5e14a6
Svitlana Usachova <svitlana.usachova@takeoff.com>
2022-02-15 19:38:31 +0200

    init commit

---
+++ test_post.py
@@ -0,0 +1,16 @@
+import json
+from urllib import request, response
+import pytest
+import requests
+
+def test_create_post():
+
+    url = 'http://localhost:5000/posts'
+    payload = { "title": "Study",
+                "text": "Go ahead",
+                "author_id": 1}
+
+    response = requests.post(url, json=payload)
+    status_code = response.status_code
+    assert status_code == 201
+
